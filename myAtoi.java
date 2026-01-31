7db17f78b28487f8d6caf3997fc31d2d0ac02fd0
Santosh <gvsantosh.05@gmail.com>
2026-01-31 23:08:52 +0530

    Update code files

---
+++ myAtoi.java
@@ -0,0 +1,32 @@
+
+
+public class myAtoi {
+
+    public static int myAtoiNum(String s) {
+        s = s.trim();
+        if (s.length() == 0) return 0;
+
+        int i = 0;
+        int sign = 1;
+        int num = 0;
+
+        if (s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        } else if (s.charAt(i) == '+') {
+            i++;
+        }
+
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            num = num * 10 + (s.charAt(i) - '0');
+            i++;
+        }
+
+        return sign * num;
+    }
+
+    public static void main(String[] args) {
+        String s = " -12345";
+        System.out.println(myAtoiNum(s)); // -12345
+    }
+}
